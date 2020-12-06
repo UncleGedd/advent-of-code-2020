f0c9d68a1eeb5f559c5ce10f366e5b88c627a6c7
unknown <gedd@example.com>
2020-12-06 10:43:10 +0000

    WIP: day 6

--- day6.py
+++ day6.py
@@ -1,2 +1,18 @@
 f = open('day6.data', 'r')
-data = f.readlines()
+data = f.read()
+data = data.split('\n\n')
+data = [d.split('\n') for d in data]
+print(data[0])
+
+answer = 0
+
+for d in data:
+    num_people = len(d)
+
+
+    
+print(answer)
+# data = [d.replace('\n', '') for d in data]
+# data = [''.join(set(d)) for d in data]
+# answer = sum(list(map(lambda x: len(x), data)))
+# print(answer)
